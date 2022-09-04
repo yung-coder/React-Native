8c0f88e6046d2142793534e4d922fc513e39527a
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-09-04 20:55:55 +0530

    native intro

--- donewithit/App.js
+++ donewithit/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
+  const handelpress = () => {
+    console.log("pressed");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2} onPress={handelpress}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem labore
+        officiis neque ipsum id modi sapiente ea ad tenetur nemo laborum
+        pariatur hic officia quas, mollitia vitae maxime debitis ipsa fuga
+        asperiores libero totam.
+      </Text>
+      <TouchableWithoutFeedback onPress={()=>console.log('image cuming')}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+        {/* <StatusBar style="auto" /> */}
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
