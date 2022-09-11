10ae299ae6ad141a488e7acd14f9e1b26063b8f6
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-09-11 16:55:30 +0530

    refreshing

--- Learning/app1/App.js
+++ Learning/app1/App.js
@@ -1,56 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Button, Linking} from 'react-native';
-import React , {useState} from 'react'
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Linking,
+  ScrollView,
+  RefreshControl,
+} from "react-native";
+import React, { useState } from "react";
 export default function App() {
-  const [name, setname] = useState('jack')
+  const [name, setname] = useState("jack");
+  const [Refreshing, setrefreshing] = useState(false);
   return (
-    <View style={styles.container}>
-      {/* <Text>{name}</Text>
-      <Button title='check' onPress={()=>{Linking.openURL('https://github.com/yung-coder')}}></Button>
-      <Button title='click' onPress={() => {setname('pika')}}>lick</Button> 
-      */}
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>1</Text>
-      </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>1</Text>
-      </View>
-    </View>
+    <ScrollView 
+       refreshControl={
+         <RefreshControl 
+            refreshing={Refreshing}
+         />
+  
+       }
+    />
+
+    // <View style={styles.container}>
+    //   {/* <Text>{name}</Text>
+    //   <Button title='check' onPress={()=>{Linking.openURL('https://github.com/yung-coder')}}></Button>
+    //   <Button title='click' onPress={() => {setname('pika')}}>lick</Button> 
+    //   */}
+    //   <View style={styles.view1}>
+        
+    //   </View>
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  view1:{
-    width: 100,
-    height: 100,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+    color: "white",
   },
-  view2:{
-    width: 100,
-    height: 100,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3:{
-    width: 100,
-    height: 100,
-    backgroundColor: 'grey',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color: 'white',
-  }
 });
