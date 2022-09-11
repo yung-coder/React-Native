768eba69fd4ca1f0668eee656c8e5b16d5fa66b3
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-09-11 16:39:51 +0530

    flex

--- Learning/app1/App.js
+++ Learning/app1/App.js
@@ -5,9 +5,19 @@ export default function App() {
   const [name, setname] = useState('jack')
   return (
     <View style={styles.container}>
-      <Text>{name}</Text>
+      {/* <Text>{name}</Text>
       <Button title='check' onPress={()=>{Linking.openURL('https://github.com/yung-coder')}}></Button>
-      <Button title='click' onPress={() => {setname('pika')}}>lick</Button>
+      <Button title='click' onPress={() => {setname('pika')}}>lick</Button> 
+      */}
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>1</Text>
+      </View>
     </View>
   );
 }
@@ -15,10 +25,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 10,
-    borderColor: 'red',
   },
+  view1:{
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2:{
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3:{
+    width: 100,
+    height: 100,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color: 'white',
+  }
 });
