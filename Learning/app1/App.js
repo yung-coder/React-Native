91cf8db89687d579d167e30f2e1458434b755888
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-09-11 17:02:33 +0530

    flat list

--- Learning/app1/App.js
+++ Learning/app1/App.js
@@ -7,19 +7,36 @@ import {
   Linking,
   ScrollView,
   RefreshControl,
+  FlatList,
 } from "react-native";
 import React, { useState } from "react";
 export default function App() {
   const [name, setname] = useState("jack");
   const [Refreshing, setrefreshing] = useState(false);
+  const [itmes, setitems] = useState([
+     {Key: 1, item: 'Item 1'},
+     {Key: 2, item: 'Item 2'},
+     {Key: 3, item: 'Item 3'},
+     {Key: 3, item: 'Item 4'},
+     {Key: 5, item: 'Item 5'},
+  ])
   return (
-    <ScrollView 
-       refreshControl={
-         <RefreshControl 
-            refreshing={Refreshing}
-         />
+    // <ScrollView 
+    //    refreshControl={
+    //      <RefreshControl 
+    //         refreshing={Refreshing}
+    //      />
   
-       }
+    //    }
+    // />
+
+    <FlatList 
+      data={itmes}
+      renderItem={({item}) =>(
+         <View key={item.Key}>
+           <Text>{item.Key}</Text>
+         </View>
+      )}
     />
 
     // <View style={styles.container}>
