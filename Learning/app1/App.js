fa1c10a73c0f057d9e30efa8244b8d261478188b
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-09-11 16:11:02 +0530

    to enter link

--- Learning/app1/App.js
+++ Learning/app1/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button, Linking} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Check</Text>
+      <Button title='check' onPress={()=>{Linking.openURL('https://github.com/yung-coder')}}></Button>
     </View>
   );
 }
