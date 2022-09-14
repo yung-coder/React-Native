f7e052c585d7b01ead1b6751ada483a03563d076
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-09-14 23:04:02 +0530

    toast alert

--- Learning/app1/App.js
+++ Learning/app1/App.js
@@ -10,41 +10,47 @@ import {
   FlatList,
   TextInput,
   TouchableWithoutFeedback,
-  TouchableHighlight
+  TouchableHighlight,
+  ToastAndroid,
+  Alert,
 } from "react-native";
 import React, { useState } from "react";
 export default function App() {
-  const [sumbited , setsumbited] = useState(false);
-  const onPressHandler = ()=>{
-    setsumbited(!sumbited);
-  }
+  const [name, setname] = useState("");
+  const [sumbited, setsumbited] = useState(false);
+  const onPressHandler = () => {
+    if (name.length > 3) {
+      setsumbited(!sumbited);
+    } else {
+      ToastAndroid.show('check it' , ToastAndroid.LONG)
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Check
-      </Text>
-    <TextInput style={styles.input} placeholder='cum'> 
-    </TextInput>
-    {/* <Button title="Sumbit"  onPress={onPressHandler}>
-    </Button> */}
-    <TouchableWithoutFeedback onPress={onPressHandler} >
-      <View style={styles.button}>
-        <Text>
-          {sumbited ? 'clear' : 'Sumbit'}
-        </Text>
-      </View>
-    </TouchableWithoutFeedback >
+      <Text style={styles.text}>Check</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="cum"
+        onChangeText={(value) => setname(value)}
+      ></TextInput>
+      <Button title="Sumbit" onPress={onPressHandler}></Button>
+      {sumbited ? <Text>{name}</Text> : null }
+      {/* <TouchableWithoutFeedback onPress={onPressHandler}>
+        <View style={styles.button}>
+          <Text>{sumbited ? "clear" : "Sumbit"}</Text>
+        </View>
+      </TouchableWithoutFeedback> */}
     </View>
-    // <ScrollView 
+    // <ScrollView
     //    refreshControl={
-    //      <RefreshControl 
+    //      <RefreshControl
     //         refreshing={Refreshing}
     //      />
-  
+
     //    }
     // />
 
-    // <FlatList 
+    // <FlatList
     //   keyExtractor={(itmes , index) => index.toString()}
     //   data={itmes}
     //   renderItem={({item}) =>(
@@ -53,17 +59,15 @@ export default function App() {
     //      </View>
     //   )}
 
-
-
     // section list also see
 
     // <View style={styles.container}>
     //   {/* <Text>{name}</Text>
     //   <Button title='check' onPress={()=>{Linking.openURL('https://github.com/yung-coder')}}></Button>
-    //   <Button title='click' onPress={() => {setname('pika')}}>lick</Button> 
+    //   <Button title='click' onPress={() => {setname('pika')}}>lick</Button>
     //   */}
     //   <View style={styles.view1}>
-        
+
     //   </View>
     // </View>
   );
@@ -79,16 +83,16 @@ const styles = StyleSheet.create({
   text: {
     color: "black",
   },
-  input:{
+  input: {
     width: 200,
     borderWidth: 1,
-    borderColor: '#555',
+    borderColor: "#555",
     borderRadius: 5,
-    textAlign:'center',
-    fontSize: 20
+    textAlign: "center",
+    fontSize: 20,
   },
-  button:{
-    color: 'black',
-    backgroundColor: 'green',
-  }
+  button: {
+    color: "black",
+    backgroundColor: "green",
+  },
 });
