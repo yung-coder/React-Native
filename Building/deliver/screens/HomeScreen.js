ac32f7df4bc975e893695eb839e351348d4d9150
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-15 17:30:14 +0530

    header

--- Building/deliver/screens/HomeScreen.js
+++ Building/deliver/screens/HomeScreen.js
@@ -1,8 +1,8 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ChevronDownIcon as ChevronDownIcon } from "react-native-heroicons/outline";
+import { AdjustmentsHorizontalIcon, ChevronDownIcon as ChevronDownIcon, MagnifyingGlassCircleIcon, MagnifyingGlassIcon, UserIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,23 +13,34 @@ const HomeScreen = () => {
     });
   }, []);
   return (
-    <SafeAreaView>
-      <Text className="text-green-500">
-        <View className='flex-row items-center mx-4 space-x-2'>
+    <SafeAreaView className='bg-white pt-5'>
+        <View className='flex-row items-center mx-4 space-x-3 pb-3'>
             <Image 
               source={{
                 uri: 'https://cdn.dribbble.com/users/1365713/screenshots/5381232/foodiction.png'
               }}
               className='h-7 w-7 bg-gray-200 p-4 rounded-full'
             />
-            <View>
+
+            <View className='flex-1'>
                 <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
-                <Text className='font-bold text-xl'>Current Location
+                <Text className='font-bold text-xl'>
+                  Current Location
                   <ChevronDownIcon size={20} color='blue'/>
                 </Text>
             </View>
+
+            <UserIcon size={20} color='blue'/>
         </View>
-      </Text>
+
+        {/* search */}
+        <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'>
+           <View className='flex-row flex-1 space-x-2  bg-gray-200 p-3'>
+             <MagnifyingGlassIcon color='blue'/>
+             <TextInput placeholder="Restaurants"/>
+           </View>
+           <AdjustmentsHorizontalIcon  color='blue'/>
+        </View>
     </SafeAreaView>
   );
 };
