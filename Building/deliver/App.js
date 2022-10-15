9a14bf1800e011992ef5340cdd284d5c8471ded9
yung-coder <chandel.sanjeev5@gmail.com>
2022-10-15 13:44:51 +0530

    project init

---
+++ Building/deliver/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
