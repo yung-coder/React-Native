65e86a71e152ac7e1a7d3452bf6da063cff4d8c6
yung-coder <chandel.sanjeev5@gmail.com>
2022-11-08 19:52:27 +0530

    head

--- Building/deliver/screens/ResturantScreen.js
+++ Building/deliver/screens/ResturantScreen.js
@@ -7,8 +7,12 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { ArrowLeftIcon } from "react-native-heroicons/outline";
-import { Text } from "react-native-svg";
+import {
+  ArrowLeftIcon,
+  StarIcon,
+  MapPinIcon,
+} from "react-native-heroicons/outline";
+import { Text } from "react-native";
 import { urlFor } from "../sanity";
 
 const ResturantScreen = () => {
@@ -44,18 +48,64 @@ const ResturantScreen = () => {
           }}
           resizeMode={"cover"}
         />
+        <TouchableOpacity style={styles.buttonopc} onPress={navigation.goBack}>
+          <ArrowLeftIcon size={20} color="#00CCBB" />
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        style={styles.buttonopc}
-        onPress={navigation.goBack}
-      >
-        <ArrowLeftIcon size={20} color="#00CCBB" />
-      </TouchableOpacity>
+      <View style={styles.description}>
+        <View style={styles.descriptionhead}>
+          <Text style={styles.descriptiontext}>{gener}</Text>
+        </View>
+        <View style={styles.descriptionheadtwo}>
+          <View style={styles.descriptioninnerhead}>
+            <StarIcon color="green" opacity={0.5} size={22} />
+            <Text style={styles.descriptionlighttext}>
+              <Text style={styles.descriptionlighttextr}>{rating}</Text>
+              {gener}
+            </Text>
+          </View>
+
+          <View style={styles.descriptioninnerhead}>
+            <MapPinIcon color="green" opacity={0.5} size={22} />
+            <Text style={styles.descriptionlighttext}>Nearby. {addres}</Text>
+          </View>
+        </View>
+      </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  description: {
+    backgroundColor: "white",
+  },
+  descriptionhead: {
+    padding: 4,
+    paddingTop: 4,
+  },
+  descriptiontext: {
+    fontSize: 34,
+    fontWeight: "bold",
+  },
+  descriptionlighttext: {
+    fontSize: 11,
+    color: "gray",
+  },
+  descriptionlighttextr: {
+    fontSize: 11,
+    color: "green",
+  },
+  descriptionheadtwo: {
+    display: "flex",
+    flexDirection: "row",
+    padding: 5,
+  },
+  descriptioninnerhead: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 5,
+  },
   buttonopc: {
     position: "absolute",
     top: 40,
