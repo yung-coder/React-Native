4aa9e80f59890eb1f13b26682d322971af634bbf
yung-coder <chandel.sanjeev5@gmail.com>
2022-11-10 22:50:53 +0530

    basket button

---
+++ Building/deliver/components/BasketIcon.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectBasketItems, selectBasketTotal } from "../features/basketSlice";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Currency from "react-currency-formatter";
+const BasketIcon = () => {
+  const items = useSelector(selectBasketItems);
+  const navigation = useNavigation();
+  const basketotal = useSelector(selectBasketTotal);
+  return (
+    <View style={styles.main}>
+      <TouchableOpacity style={styles.hero}>
+        <Text style={styles.text1}>{items.length}</Text>
+        <Text style={styles.text2}>View Basket</Text>
+        <Text styles={styles.mainsec}>
+          <Currency quantity={basketotal} currency="GBP" />
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    main: {
+        width: 359,
+        position: "absolute",
+        bottom: 10,
+        zIndex: 20,
+    },
+    hero: {
+      margin: 25,
+      padding: 10,
+      borderRadius: 60,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: "blue",
+    },
+  mainsec: {
+    color: "white",
+    fontSize: 25,
+    fontWeight: "400",
+  },
+  text1:{
+    color: 'white',
+    fontWeight: '800',
+    fontSize: 15,
+    padding: 4,
+    borderRadius: 15,
+    backgroundColor: 'gray'
+  },
+  text2:{
+    color: 'white',
+    fontWeight: '400',
+    fontSize: 15,
+    padding: 4,
+    alignItems: 'center'
+  }
+});
+
+export default BasketIcon;
--- Building/deliver/screens/ResturantScreen.js
+++ Building/deliver/screens/ResturantScreen.js
@@ -17,6 +17,7 @@ import {
 import { Text } from "react-native";
 import { urlFor } from "../sanity";
 import Dishrow from "../components/Dishrow";
+import BasketIcon from "../components/BasketIcon";
 
 const ResturantScreen = () => {
   const navigation = useNavigation();
@@ -42,61 +43,68 @@ const ResturantScreen = () => {
   }, []);
 
   return (
-    <ScrollView>
-      <View style={styles.cont}>
-        <Image
-          style={styles.image}
-          source={{
-            uri: urlFor(imgUrl).url(),
-          }}
-          resizeMode={"cover"}
-        />
-        <TouchableOpacity style={styles.buttonopc} onPress={navigation.goBack}>
-          <ArrowLeftIcon size={20} color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.description}>
-        <View style={styles.descriptionhead}>
-          <Text style={styles.descriptiontext}>{gener}</Text>
-        </View>
-        <View style={styles.descriptionheadtwo}>
-          <View style={styles.descriptioninnerhead}>
-            <StarIcon color="green" opacity={0.5} size={22} />
-            <Text style={styles.descriptionlighttext}>
-              <Text style={styles.descriptionlighttextr}>{rating}</Text>
-              {gener}
-            </Text>
-          </View>
+    <>
+    <BasketIcon />
 
-          <View style={styles.descriptioninnerhead}>
-            <MapPinIcon color="green" opacity={0.5} size={22} />
-            <Text style={styles.descriptionlighttext}>Nearby. {addres}</Text>
-          </View>
+      <ScrollView>
+        <View style={styles.cont}>
+          <Image
+            style={styles.image}
+            source={{
+              uri: urlFor(imgUrl).url(),
+            }}
+            resizeMode={"cover"}
+          />
+          <TouchableOpacity
+            style={styles.buttonopc}
+            onPress={navigation.goBack}
+          >
+            <ArrowLeftIcon size={20} color="#00CCBB" />
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.additionalbtn}>
-          <QuestionMarkCircleIcon color="gray" opacity={0.5} size={20} />
-          <Text style={styles.addintiontext}>Have a food !</Text>
-          <ChevronRightIcon color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text style={styles.textMenu}>Menu</Text>
+        <View style={styles.description}>
+          <View style={styles.descriptionhead}>
+            <Text style={styles.descriptiontext}>{gener}</Text>
+          </View>
+          <View style={styles.descriptionheadtwo}>
+            <View style={styles.descriptioninnerhead}>
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text style={styles.descriptionlighttext}>
+                <Text style={styles.descriptionlighttextr}>{rating}</Text>
+                {gener}
+              </Text>
+            </View>
 
-        {/* Dishrow */}
-        {dishes.map((dish) => {
-          return (
-            <Dishrow
-              key={dish._id}
-              id={dish._id}
-              name={dish.name}
-              description={dish.short_des}
-              price={dish.price}
-              image={dish.image}
-            />
-          );
-        })}
-      </View>
-    </ScrollView>
+            <View style={styles.descriptioninnerhead}>
+              <MapPinIcon color="green" opacity={0.5} size={22} />
+              <Text style={styles.descriptionlighttext}>Nearby. {addres}</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.additionalbtn}>
+            <QuestionMarkCircleIcon color="gray" opacity={0.5} size={20} />
+            <Text style={styles.addintiontext}>Have a food !</Text>
+            <ChevronRightIcon color="#00CCBB" />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.exception}>
+          <Text style={styles.textMenu}>Menu</Text>
+
+          {/* Dishrow */}
+          {dishes.map((dish) => {
+            return (
+              <Dishrow
+                key={dish._id}
+                id={dish._id}
+                name={dish.name}
+                description={dish.short_des}
+                price={dish.price}
+                image={dish.image}
+              />
+            );
+          })}
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
@@ -166,6 +174,9 @@ const styles = StyleSheet.create({
     padding: 4,
     width: "100%",
   },
+  exception:{
+    paddingBottom: 30,
+  }
 });
 
 export default ResturantScreen;
